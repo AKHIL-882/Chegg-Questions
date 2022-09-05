598234453e87aadfc89ab3cdfce4c1dc72e480b9
Duggirala Akhil <51040667+AKHIL-882@users.noreply.github.com>
2022-09-05 06:20:10 +0530

    Add files via upload

---
+++ C++ programs/1. Multi Dimentional Array/Average of Three numbers.cpp
@@ -0,0 +1,17 @@
+// importing the libraries
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+// main method
+int main() {
+   // declaring the variables
+   double num_1,num_2,num_3;
+   // getting user input 
+   cin>>num_1>>num_2>>num_3;
+   // calculating the average of there numbers
+   double average = (num_1 + num_2 + num_3) / 3;
+   // displaying the result
+   cout << "Average is " << setprecision(4) <<average;
+   cout << endl;
+   
+}
