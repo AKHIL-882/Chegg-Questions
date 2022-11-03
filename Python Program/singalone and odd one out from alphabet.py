01e7357182c7a96a119b7e479b216c711007aa49
Duggirala Akhil <51040667+AKHIL-882@users.noreply.github.com>
2022-11-03 21:12:38 +0530

    Add files via upload

---
+++ Python Program/singalone and odd one out from alphabet.py
@@ -0,0 +1,55 @@
+
+# singalOne =[int (n) for n in input ().split ()]
+# singalTwo =[int (n) for n in input ().split ()]
+# count = 0
+# for i in range(len(singalOne)):
+#     for j in range(len(singalTwo)):
+#         print(singalOne[i],singalTwo[i])
+#         if(singalOne[i]==singalTwo[i]):
+#             count=count+1
+#             if(count==singalOne[i]):
+#                 count=count
+#             else:
+#                 count=singalOne[i]
+#         break
+#     continue
+# print(count)
+
+
+
+
+# series = ["ACB" , "BDC" , "CED" , "DEF"]
+# l = []
+# sol = []
+# for i in series:
+#     for j in i:
+#         l.append(j)
+# print(l)
+
+
+# def findOdd(series)
+#     l = []
+#     for s in sr:
+#         r= ord(s[1])-ord(s[0]) +ord(s[2])-ord(s[1])
+#         l.append(r)
+#     pos = l[0]
+#     for i in range(len(l)):
+#         if(l[i]!=pos):
+#             return sr[i]
+    
+    
+    
+    
+# word = "as?b?e?gf"
+# substr = "dbk"
+# count=0
+# for i in range(len(word)):
+#     if(word[i]=='?'):
+#         new = word[i].replace(substr[i:])
+#         for j in substr:
+#             new = word.replace(word[i],j)
+#         count=count+1
+# print(new)
+
+
+
